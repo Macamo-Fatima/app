77e80fcc31f894fcb323a1cd31241a77fd9290c7
Macamo-Fatima <92531916+Macamo-Fatima@users.noreply.github.com>
2025-02-22 06:15:58 +0200

    Update login002.py

--- login002.py
+++ login002.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 from io import BytesIO, StringIO
 from decouple import config
 from langchain import PromptTemplate
